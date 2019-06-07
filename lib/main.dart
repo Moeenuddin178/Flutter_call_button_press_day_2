b3ed552489dae3430b9e8ed60f23637d05f6776d
Moeenuddin <moeenuddin178@gmail.com>
2019-06-07 10:06:42 +0500

    addaded a library in pubspec for working of this app

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+    appBar: new AppBar(
+      title: new Text("View"),
+    ),
+    body: InkWell(
+      child: new FlatButton(
+          onPressed: () => launch("tel://21213123123"),
+          child: new Text("Call me")),
+      splashColor: Colors.amberAccent,
+    ),
+  );
+}
+
+void main() {
+  runApp(
+    new MyApp(),
+  );
+}
